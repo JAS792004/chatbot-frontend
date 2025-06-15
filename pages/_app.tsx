16b6ef50b4c7f1ecfb4e1e0f5d581ba9f9367b52
Jyothiraditya Singh <jyothiraditya.singh@s.amity.edu>
2025-06-15 14:36:43 +0530

    Tailwind config finalized

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
+import "@/styles/globals.css"; // or "../styles/globals.css" if alias fails
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
